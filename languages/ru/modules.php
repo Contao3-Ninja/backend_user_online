c33a77fc311fe7f56b354d921218b654cdef910c
Glen Langer <contao.modul@glen-langer.de>
2014-11-01 23:04:03 +0100

    Update Transifex Language Files

---
+++ languages/ru/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-backend_user_online/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-11-01T23:02:19+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['backend_user_online']['0'] = 'Пользователь панели управления онлайн';
+$GLOBALS['TL_LANG']['MOD']['backend_user_online']['1'] = 'Иконка онлайн-статуса для пользователя панели управления и внешнего интерфейса сайта';
+
